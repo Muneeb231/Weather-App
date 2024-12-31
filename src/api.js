85a4c11539e523062e227fa24d9f4b73087db217
Muneeb231 <122695268+Muneeb231@users.noreply.github.com>
2024-12-31 16:35:32 -0500

    Created the search bar for weather app

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'da93bd1266msh28ecc7c1663f050p15ef0ejsn8db57d74a65b',
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
