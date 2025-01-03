84f8347b6dd8f6a4391ee433c1051b8767ec9dcc
Muneeb Saeed <122695268+Muneeb231@users.noreply.github.com>
2025-01-02 22:41:01 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': 'da93bd1266msh28ecc7c1663f050p15ef0ejsn8db57d74a65b',
+        'x-rapidapi-key': 'MY_UNIQUE_API_KEY',
         'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
     }
 };
